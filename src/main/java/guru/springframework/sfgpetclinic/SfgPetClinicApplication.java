d940ef8f380e50bbec4768d089022ed9de98a81c
Philip Alb <philip.alb@gmx.de>
2018-10-03 10:48:59 +0200

    Just added some output to check Git Issue #2 connection.

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -8,5 +8,6 @@ public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
+		System.out.println("Just to check Git stuff...");
 	}
 }
