fb81733b0c77f89a7e52257c557c65f40e10d233
Philip Alb <philip.alb@gmx.de>
2018-10-03 10:57:39 +0200

    Test

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -8,6 +8,6 @@ public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
-		System.out.println("Just to check Git stuff...");
+		System.out.println("Just to check Git stuff.....");
 	}
 }
