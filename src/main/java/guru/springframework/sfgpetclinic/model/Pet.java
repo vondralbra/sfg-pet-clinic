ac3ea947eab49a90caac8bce8d9bc5a009938597
Philip Alb <philip.alb@gmx.de>
2018-10-04 12:31:48 +0200

    #2 Add pojo datamodel

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+	private PetType petTYpe;
+	private LocalDate birthday;
+	private Owner owner;
+
+	public PetType getPetTYpe() {
+		return petTYpe;
+	}
+
+	public void setPetTYpe(PetType petTYpe) {
+		this.petTYpe = petTYpe;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+}
