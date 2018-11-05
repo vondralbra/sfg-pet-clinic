e8dd5b5f376102282f49d5db46dd3537509a3224
Philip Alb <philip.alb@gmx.de>
2018-11-05 10:15:22 +0100

    Convert Vets to JPA Entities. close #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
 public class Speciality extends BaseEntity {
 
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
